73148ec7ad424614292de1059cc5578958c23b35
박현진 <hyunjin@petch.com>
2024-10-22 15:19:00 +0900

    Feat: 코틀린 함수, 변수 정리

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,8 @@
 fun main() {
     val value:Int = max(5, 3)
     println("Max value is : $value")
+
+    println("Max value is : ${max(5, 3)}")
 }
 
 fun max(a:Int, b:Int):Int = if (a > b) a else b // option + enter 누르면 이렇게 한 줄로 바꿔준다.
