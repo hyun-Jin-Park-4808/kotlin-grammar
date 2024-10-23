c25dbd619bc342fc0d02b0f309be1333701280ad
박현진 <hyunjin@petch.com>
2024-10-23 16:01:46 +0900

    Feat: 코틀린 클래스, 프로퍼티 정리

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,14 @@
 fun main() {
-    val value:Int = max(5, 3)
+    val value: Int = max(5, 3)
     println("Max value is : $value")
 
     println("Max value is : ${max(5, 3)}")
+
+    val person = Person("November", false)
+    person.name = "new Name"
+    person.isMarried = true
+
+    println("person name : ${person.name}, isMarried: ${person.isMarried}")
 }
 
-fun max(a:Int, b:Int):Int = if (a > b) a else b // option + enter 누르면 이렇게 한 줄로 바꿔준다.
+fun max(a: Int, b: Int):Int = if (a > b) a else b // option + enter 누르면 이렇게 한 줄로 바꿔준다.
---
+++ src/Person.kt
@@ -0,0 +1,14 @@
+class Person(
+    var name: String,
+    var isMarried: Boolean
+)
+
+// val로 작성하면 new로 생성된 Person 객체의 속성값 못 바꾼다.
+//class Person(
+//    val name: String,
+//    val isMarried: Boolean
+//)
+
+class Family(
+    val familyName: String
+)
