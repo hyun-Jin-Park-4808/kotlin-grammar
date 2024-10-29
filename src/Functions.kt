4a68dc7ff0861dd799fc18e02bf937179f39ddca
박현진 <hyunjin@petch.com>
2024-10-29 10:08:10 +0900

    Feat: 함수 정리2

--- src/Functions.kt
+++ src/Functions.kt
@@ -5,12 +5,23 @@ val map = mapOf(
     3 to "Three"
 )
 
+fun String.double(add: String) = this + this + add
+
+infix fun String.add(postfix: String) = this + postfix
+
+infix fun String.equalTo(expected: String) =
+    this == expected
+
 fun getMyName(
     firstName: String = "foo",
     lastName: String = "bar"
 ): String = "$firstName $lastName"
 
 fun main() {
+    println("This is very ".add("good"))
+    println("This is very " add "good")
+    println("xxxy" equalTo "xxxy")
+    println("do".double(" is the best"))
     println(set.javaClass)
     println(list.javaClass)
     println(map.javaClass)
@@ -20,4 +31,25 @@ fun main() {
     println(getMyName())
     println(getMyName("Steve"))
     println(getMyName(lastName = "Jobs"))
+
+    val person = Pair("Snow", true)
+    val (name, isMarried) = person
+
+    val persons = mapOf(
+        "Snow" to true,
+        "Peter" to false,
+        "Paul" to true,
+    )
+
+// map iteration 시에 분해해서 접근
+    persons.forEach {
+            (name, isMarried) ->
+        println("$name is married($isMarried)")
+    }
+
+// isMarried 미사용
+    persons.forEach {
+            (name, _) ->
+        println("$name is in this list")
+    }
 }
